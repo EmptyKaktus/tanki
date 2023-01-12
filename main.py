96d522e98dd8cefaf9cc9ec4f8247f668cce94ed
EmptyKaktus <gabdrafikovs7@gmail.com>
2023-01-12 18:32:17 +0300

    1

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+
+SCREEN_SIZE1 = [1540, 810]
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 10
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, 'white', (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                    self.cell_size), 1)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = SCREEN_SIZE1
+    screen = pygame.display.set_mode(size)
+    board = Board(154, 81)
+    board.set_view(1, 1, )
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        board.render(screen)
+        pygame.display.flip()
+    pygame.quit()

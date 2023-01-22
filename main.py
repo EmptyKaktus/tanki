f1e5c3024f2ab440988677f845ecd5988d283a98
Shamil <89793572+EmptyKaktus@users.noreply.github.com>
2023-01-23 00:01:53 +0300

    ограничено движение танка

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ tile_width = tile_height = 30
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+wall_group = pygame.sprite.Group()
+mine_group = pygame.sprite.Group()
+river_group = pygame.sprite.Group()
+bridge_group = pygame.sprite.Group()
+home_group = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -85,9 +90,11 @@ class Tank1(pygame.sprite.Sprite):
         self.direct = direct
         self.moveSpeed = 1
         self.image = tanks_images['1']
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = pos_x * 30
         self.rect.y = pos_y * 30
+        self.q1, self.q2, self.q3, self.q4 = True, True, True, True
 
         self.keyLEFT = keyList[0]
         self.keyRIGHT = keyList[1]
@@ -96,30 +103,69 @@ class Tank1(pygame.sprite.Sprite):
         self.keySHOT = keyList[4]
 
     def update(self):
+        wall = pygame.sprite.spritecollideany(self, wall_group)
+        river = pygame.sprite.spritecollideany(self, river_group)
+        bridge = pygame.sprite.spritecollideany(self, bridge_group)
         if keys[self.keyLEFT]:
             if self.direct != 3:
                 self.image = tanks_images['1']
+                self.mask = pygame.mask.from_surface(self.image)
                 self.image = pygame.transform.flip(self.image, True, True)
                 self.direct = 3
-            self.rect.x -= self.moveSpeed
+            if (wall or river) and not bridge:
+                self.q3 = False
+                self.rect.x += self.moveSpeed
+            if self.q3:
+                self.rect.x -= self.moveSpeed
+                self.q1 = True
+                self.q4 = True
+                self.q2 = True
+                self.q3 = True
         elif keys[self.keyRIGHT]:
             if self.direct != 1:
                 self.image = tanks_images['1']
+                self.mask = pygame.mask.from_surface(self.image)
                 self.image = pygame.transform.flip(self.image, False, True)
                 self.direct = 1
-            self.rect.x += self.moveSpeed
+            if (wall or river) and not bridge:
+                self.q1 = False
+                self.rect.x -= self.moveSpeed
+            if self.q1:
+                self.rect.x += self.moveSpeed
+                self.q3 = True
+                self.q4 = True
+                self.q1 = True
+                self.q2 = True
         elif keys[self.keyUP]:
             if self.direct != 2:
                 self.image = tanks_images['1c']
+                self.mask = pygame.mask.from_surface(self.image)
                 self.direct = 2
-            self.rect.y -= self.moveSpeed
+            if (wall or river) and not bridge:
+                self.q2 = False
+                self.rect.y += self.moveSpeed
+            if self.q2:
+                self.rect.y -= self.moveSpeed
+                self.q4 = True
+                self.q1 = True
+                self.q2 = True
+                self.q3 = True
         elif keys[self.keyDOWN]:
             self.image = tanks_images['1c']
             self.direct = 2
             if self.direct != 4:
+                self.mask = pygame.mask.from_surface(self.image)
                 self.image = pygame.transform.flip(self.image, False, True)
                 self.direct = 4
-            self.rect.y += self.moveSpeed
+            if (wall or river) and not bridge:
+                self.q4 = False
+                self.rect.y -= self.moveSpeed
+            if self.q4:
+                self.rect.y += self.moveSpeed
+                self.q2 = True
+                self.q1 = True
+                self.q4 = True
+                self.q3 = True
 
 
 class Tank2(pygame.sprite.Sprite):
@@ -141,7 +187,7 @@ class Tank2(pygame.sprite.Sprite):
         self.keyDOWN = keyList[3]
         self.keySHOT = keyList[4]
 
-    def update(self):
+    def update(self, ):
         if keys[self.keyLEFT]:
             if self.direct != 3:
                 self.image = tanks_images['1']
@@ -168,6 +214,44 @@ class Tank2(pygame.sprite.Sprite):
             self.rect.y += self.moveSpeed
 
 
+class Mine(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(mine_group, all_sprites)
+        pass
+
+    def boom(self):
+        pass
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(wall_group, all_sprites)
+        self.image = tile_images['S']
+        self.rect = self.image.get_rect().move(
+            tile_width * x, tile_height * y)
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def crash(self):
+        pass
+
+
+class River(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(river_group, all_sprites)
+        self.image = tile_images['W']
+        self.rect = self.image.get_rect().move(
+            tile_width * x, tile_height * y)
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Bridge(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(bridge_group, all_sprites)
+        self.image = tile_images['M']
+        self.rect = self.image.get_rect().move(
+            tile_width * x, tile_height * y)
+        self.mask = pygame.mask.from_surface(self.image)
+
 
 # Генерируем тарву и реку
 def generate_level_1(level):
@@ -175,21 +259,21 @@ def generate_level_1(level):
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == 'W' or level[y][x] == 'M':
-                Tile('W', x, y)
-            else:
+                River(x, y)
+            if level[y][x] == 'M':
+                Bridge(x, y)
+            if level[y][x] == 'd' or level[y][x] == 'D' or level[y][x] == 'S' or level[y][x] == 'Z' or level[y][x] == '*' or level[y][x] == 'T':
                 Tile('*', x, y)
     return x, y
 
 
 # Генерируем все остальное
 def generate_level_2(level):
-    player_1, player_2, x, y = None, None, None, None
+    player_1, player_2, x, y, wall, mine = None, None, None, None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == 'S':
-                Tile('S', x, y)
-            elif level[y][x] == 'M':
-                Tile('M', x - 1, y)
+                wall = Wall(x, y)
             elif level[y][x] == 'Z':
                 Tile('Z', x, y)
             elif level[y][x] == 'K':
@@ -202,7 +286,7 @@ def generate_level_2(level):
                 Tile('d', x, y)
             elif level[y][x] == 'T':
                 player_1 = Tank1('blue', x, y, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
-    return player_1, player_2, x, y
+    return player_1, player_2, wall, mine, x, y
 
 
 class Field:
@@ -244,7 +328,7 @@ class About:
 if __name__ == '__main__':
     pygame.init()
     level_x, level_y = generate_level_1(load_level('Поле 1.txt'))
-    player1, player2, level_x1, level_y1 = generate_level_2(load_level('Поле 1.txt'))
+    player1, player2, wall, mine, level_x1, level_y1 = generate_level_2(load_level('Поле 1.txt'))
     board = Field(45, 23)
     board.set_view(1, 1, 30)
     running = True
@@ -255,6 +339,6 @@ if __name__ == '__main__':
         keys = pygame.key.get_pressed()
         all_sprites.draw(screen)
         all_sprites.update()
-        #board.render(screen)
+        # board.render(screen)
         pygame.display.flip()
     pygame.quit()

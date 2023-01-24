a59d4e9d2177817df5f7adb28a3cc109903495f0
Shamil <89793572+EmptyKaktus@users.noreply.github.com>
2023-01-24 13:45:59 +0300

    изменил траву

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ about_image = {
 }
 
 
+# Создаем класс клеточек и добавляем его в tieles_group
 class Tile(pygame.sprite.Sprite):
     papka = 'tiels'
 
@@ -81,6 +82,7 @@ class Tile(pygame.sprite.Sprite):
             tile_width * pos_x, tile_height * pos_y)
 
 
+# Создаем класс первого танка и добавляем его в player_group
 class Tank1(pygame.sprite.Sprite):
     papka = 'players'
 
@@ -89,13 +91,15 @@ class Tank1(pygame.sprite.Sprite):
         self.color = color
         self.direct = direct
         self.moveSpeed = 1
-        self.image = tanks_images['1']
+        self.image = tanks_images['2']
+        self.name = '2'
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = pos_x * 30
         self.rect.y = pos_y * 30
         self.q1, self.q2, self.q3, self.q4 = True, True, True, True
 
+        # расписываем кнопки управления
         self.keyLEFT = keyList[0]
         self.keyRIGHT = keyList[1]
         self.keyUP = keyList[2]
@@ -103,16 +107,20 @@ class Tank1(pygame.sprite.Sprite):
         self.keySHOT = keyList[4]
 
     def update(self):
+        # проверяем есть ли соприкосновения с другими объектами
         wall = pygame.sprite.spritecollideany(self, wall_group)
         river = pygame.sprite.spritecollideany(self, river_group)
         bridge = pygame.sprite.spritecollideany(self, bridge_group)
+        home = pygame.sprite.spritecollideany(self, home_group)
+
+        # Расписываем нажатия кнопок и проверку на соприкосновения
         if keys[self.keyLEFT]:
             if self.direct != 3:
-                self.image = tanks_images['1']
+                self.image = tanks_images[self.name]
                 self.mask = pygame.mask.from_surface(self.image)
                 self.image = pygame.transform.flip(self.image, True, True)
                 self.direct = 3
-            if (wall or river) and not bridge:
+            if (wall or river or home) and not bridge:
                 self.q3 = False
                 self.rect.x += self.moveSpeed
             if self.q3:
@@ -123,11 +131,11 @@ class Tank1(pygame.sprite.Sprite):
                 self.q3 = True
         elif keys[self.keyRIGHT]:
             if self.direct != 1:
-                self.image = tanks_images['1']
+                self.image = tanks_images[self.name]
                 self.mask = pygame.mask.from_surface(self.image)
                 self.image = pygame.transform.flip(self.image, False, True)
                 self.direct = 1
-            if (wall or river) and not bridge:
+            if (wall or river or home) and not bridge:
                 self.q1 = False
                 self.rect.x -= self.moveSpeed
             if self.q1:
@@ -138,10 +146,10 @@ class Tank1(pygame.sprite.Sprite):
                 self.q2 = True
         elif keys[self.keyUP]:
             if self.direct != 2:
-                self.image = tanks_images['1c']
+                self.image = tanks_images[f'{self.name}c']
                 self.mask = pygame.mask.from_surface(self.image)
                 self.direct = 2
-            if (wall or river) and not bridge:
+            if (wall or river or home) and not bridge:
                 self.q2 = False
                 self.rect.y += self.moveSpeed
             if self.q2:
@@ -151,13 +159,13 @@ class Tank1(pygame.sprite.Sprite):
                 self.q2 = True
                 self.q3 = True
         elif keys[self.keyDOWN]:
-            self.image = tanks_images['1c']
+            self.image = tanks_images[f'{self.name}c']
             self.direct = 2
             if self.direct != 4:
                 self.mask = pygame.mask.from_surface(self.image)
                 self.image = pygame.transform.flip(self.image, False, True)
                 self.direct = 4
-            if (wall or river) and not bridge:
+            if (wall or river or home) and not bridge:
                 self.q4 = False
                 self.rect.y -= self.moveSpeed
             if self.q4:
@@ -168,6 +176,7 @@ class Tank1(pygame.sprite.Sprite):
                 self.q3 = True
 
 
+# Создаем класс второго танка(игрока) и добавляем его в player_group
 class Tank2(pygame.sprite.Sprite):
     papka = 'players'
 
@@ -214,15 +223,18 @@ class Tank2(pygame.sprite.Sprite):
             self.rect.y += self.moveSpeed
 
 
+# Создаем класс мины и добавляем его в mine_group
 class Mine(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(mine_group, all_sprites)
         pass
 
+    # действия мины
     def boom(self):
         pass
 
 
+# Создаем класс стены(препятствия) и добавляем его в wall_group
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(wall_group, all_sprites)
@@ -231,10 +243,12 @@ class Wall(pygame.sprite.Sprite):
             tile_width * x, tile_height * y)
         self.mask = pygame.mask.from_surface(self.image)
 
+    # разрушение стены
     def crash(self):
         pass
 
 
+# Создаем класс реки и добавляем его в river_group
 class River(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(river_group, all_sprites)
@@ -244,6 +258,7 @@ class River(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
 
+# Создаем класс моста и добавляем его в bridge_group
 class Bridge(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(bridge_group, all_sprites)
@@ -253,7 +268,21 @@ class Bridge(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
 
-# Генерируем тарву и реку
+# Создаем класс дома и добавляем его в home_group
+class Home(pygame.sprite.Sprite):
+    def __init__(self, name, x, y):
+        super().__init__(home_group, all_sprites)
+        self.image = tile_images[name]
+        self.rect = self.image.get_rect().move(
+            tile_width * x, tile_height * y)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.image1 = tile_images[name]
+        self.rect = self.image.get_rect().move(
+            tile_width * x, tile_height * y)
+        self.mask = pygame.mask.from_surface(self.image1)
+
+
+# Генерируем траву, мост и реку
 def generate_level_1(level):
     x, y = None, None
     for y in range(len(level)):
@@ -269,11 +298,11 @@ def generate_level_1(level):
 
 # Генерируем все остальное
 def generate_level_2(level):
-    player_1, player_2, x, y, wall, mine = None, None, None, None, None, None
+    player_1, player_2, x, y= None, None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == 'S':
-                wall = Wall(x, y)
+                Wall(x, y)
             elif level[y][x] == 'Z':
                 Tile('Z', x, y)
             elif level[y][x] == 'K':
@@ -281,14 +310,14 @@ def generate_level_2(level):
             elif level[y][x] == 'k':
                 Tile('k', x, y)
             elif level[y][x] == 'D':
-                Tile('D', x, y)
+                Home('D', x, y)
             elif level[y][x] == 'd':
-                Tile('d', x, y)
+                Home('d', x, y)
             elif level[y][x] == 'T':
                 player_1 = Tank1('blue', x, y, 0, (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s, pygame.K_SPACE))
-    return player_1, player_2, wall, mine, x, y
+    return player_1, player_2, x, y
 
-
+# клетчатое поле для разметки
 class Field:
     def __init__(self, width, height):
         self.width = width
@@ -311,6 +340,7 @@ class Field:
                     self.cell_size), 1)
 
 
+# Всякие кнопочки
 class About:
     def __init__(self):
         pass
@@ -328,7 +358,7 @@ class About:
 if __name__ == '__main__':
     pygame.init()
     level_x, level_y = generate_level_1(load_level('Поле 1.txt'))
-    player1, player2, wall, mine, level_x1, level_y1 = generate_level_2(load_level('Поле 1.txt'))
+    player1, player2, level_x1, level_y1 = generate_level_2(load_level('Поле 1.txt'))
     board = Field(45, 23)
     board.set_view(1, 1, 30)
     running = True
